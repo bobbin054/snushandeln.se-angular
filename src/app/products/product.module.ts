aa2de67ddfe9b007c642c13bfcdea4aaf94c88eb
joosteli <joosteli@L-SE-5CD9124X3G>
2021-09-23 15:09:27 +0200

    refactoring

--- src/app/products/product.module.ts
+++ src/app/products/product.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { ConvertToSpacesPipe } from '../shared/convert-to-spaces.pipe';
-import { ProductDetailComponent } from './product-detail.component';
-import { ProductListComponent } from './product-list.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { ProductListComponent } from './product-list/product-list.component';
 import { RouterModule } from '@angular/router';
-import { ProductDetailGuard } from './product-detail.guard';
+import { ProductDetailGuard } from './product-detail/product-detail.guard';
 import { SharedModule } from '../shared/shared.module';
 
 
