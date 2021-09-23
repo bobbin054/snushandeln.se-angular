e80d95f5cdb624754d3127dbfc51ebf8d59616e4
joosteli <joosteli@L-SE-5CD9124X3G>
2021-09-23 14:58:01 +0200

    refactoring

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { IProduct } from './product';
 import { ProductService } from './product.service';
-
 @Component({
   templateUrl: './product-detail.component.html',
   styleUrls: ['./product-detail.component.css']
