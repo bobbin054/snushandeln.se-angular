8750e305d550c38d42f7756d06f88117a5ced43f
Joakim Östelid <joakim.ostelid@outlook.com>
2022-12-30 23:13:00 +0100

    state

--- src/app/shopping-cart-list-component/shopping-cart-list.component.ts
+++ src/app/shopping-cart-list-component/shopping-cart-list.component.ts
@@ -1,39 +1,31 @@
-import { Component, OnDestroy, OnInit } from "@angular/core";
-import { ActivatedRoute, Router } from "@angular/router";
-import { IProduct } from "../interface/product";
-import { ProductService } from "../product/product.service";
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { IProduct } from '../interface/product';
+import { ProductService } from '../product/product.service';
 
 @Component({
-    templateUrl: './shopping-cart-list.component.html',
-    styleUrls: ['./shopping-cart-list.component.css'],
-    selector: 'shopping-cart-list'
+  templateUrl: './shopping-cart-list.component.html',
+  styleUrls: ['./shopping-cart-list.component.css'],
+  selector: 'shopping-cart-list',
 })
-export class ShoppingCartListComponent
-{
-    pageTitle: string = 'Product Detail';
-    product: IProduct | undefined;
-    productId: Number | undefined;
-    public imageWidth: number = 100;
-    public imageMargin: number = 2;
-    constructor(private route: ActivatedRoute,
-        private router: Router,
-        private productService: ProductService)
-      {
+export class ShoppingCartListComponent {
+  pageTitle: string = 'Product Detail';
+  product: IProduct | undefined;
+  productId: number | undefined;
+  public imageWidth: number = 100;
+  public imageMargin: number = 2;
+  constructor(
+    private route: ActivatedRoute,
+    private productService: ProductService
+  ) {}
 
-      }
-
-    ngOnInit(): void
-  {
+  ngOnInit(): void {
     this.productId = Number(this.route.snapshot.paramMap.get('id'));
-
     this.pageTitle += `: ${this.productId}`;
-
     this.productService.getProducts().subscribe({
-      next: products =>
-      {
-        this.product = products.find(x => x.productId === this.productId);
-      }
+      next: (products) => {
+        this.product = products.find((x) => x.productId === this.productId);
+      },
     });
-
   }
 }
