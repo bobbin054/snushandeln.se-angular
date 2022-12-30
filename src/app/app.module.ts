83050b018cd3b6e94c102943a7094a65c569a590
Joakim Östelid <joakim.ostelid@outlook.com>
2022-12-30 21:46:03 +0100

    state

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,15 +7,17 @@ import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome-component/welcome.component';
 import { ProductModule } from './product/product.module';
 import { ShoppingCartListComponent } from './shopping-cart-list-component/shopping-cart-list.component';
+import { ShopComponent } from './shop/shop.component';
 
 @NgModule({
-  declarations: [AppComponent, WelcomeComponent],
+  declarations: [AppComponent, WelcomeComponent,ShopComponent],
   imports: [
     BrowserModule,
     HttpClientModule,
     ProductModule,
     RouterModule.forRoot([
       { path: 'welcome', component: WelcomeComponent },
+      { path: 'shop', component: ShopComponent },
       { path: 'shopping-cart', component: ShoppingCartListComponent },
       { path: '', redirectTo: 'welcome', pathMatch: 'full' },
       { path: '**', redirectTo: 'welcome', pathMatch: 'full' },
---
+++ src/app/shop/shop.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: './shop.component.html',
+  styleUrls: ['./shop.component.css'],
+})
+export class ShopComponent {
+  constructor() {}
+}
