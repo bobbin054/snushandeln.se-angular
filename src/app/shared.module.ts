b9072a16248088b06c15c5edc8a950a6396949c5
Joakim Östelid <joakim.ostelid@outlook.com>
2022-12-30 23:47:05 +0100

    state

--- src/app/shared.module.ts
+++ src/app/shared.module.ts
@@ -1,20 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
 import { StarComponent } from './star-component/star.component';
 
 @NgModule({
-  declarations: [
-    StarComponent
-  ],
-  imports: [
-    CommonModule
-  ],
-  exports:[
-    StarComponent,
-    CommonModule,
-    FormsModule
-  ]
+  declarations: [StarComponent],
+  imports: [CommonModule],
+  exports: [StarComponent, CommonModule, FormsModule],
 })
-export class SharedModule { }
+export class SharedModule {}
