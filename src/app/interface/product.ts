f48d97bf5ab4377a4d0edfad3bd4c32ce9513a26
Joakim Östelid <joakim.ostelid@outlook.com>
2022-12-31 13:27:12 +0100

    state

--- src/app/interface/product.ts
+++ src/app/interface/product.ts
@@ -1,18 +1,15 @@
-export interface IProduct
-{
-    "productId": number;
-    "productName": string;
-    "type": string;
-    "nicotineLevel": string;
-    "nicotineAmount": string;
-    "taste": string;
-    "contentWeight": number;
-    "contentWeightUnit": string;
-    "price": number;
-    "starRating": number;
-    "imageUrl": string;
-    "producent": string
-    "description": string;
+export interface IProduct {
+  productId: number;
+  productName: string;
+  type: string;
+  nicotineLevel: string;
+  nicotineAmount: string;
+  taste: string;
+  contentWeight: number;
+  contentWeightUnit: string;
+  price: number;
+  starRating: number;
+  imageUrl: string;
+  producent: string;
+  description: string;
 }
-
-
