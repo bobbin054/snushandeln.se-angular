a19993f12f0116fcd6888b870915b6cac90b0e38
Joakim Östelid <joakim.ostelid@outlook.com>
2023-01-17 22:00:17 +0100

    state

--- src/app/components/product/product-list-component/product-list.component.ts
+++ src/app/components/product/product-list-component/product-list.component.ts
@@ -3,22 +3,22 @@ import { Subscription } from 'rxjs';
 import { CartService } from 'src/app/services/cart.service';
 import { IProduct } from './../../../interface/product';
 import { ProductService } from './../../../services/product.service';
+
 @Component({
   selector: 'pm-product-list',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.scss'],
 })
 export class ProductListComponent implements OnInit, OnDestroy {
-  public pageTitle: string = 'Products';
   public showImage: boolean = true;
   public sub!: Subscription;
   public filteredProducts: IProduct[] = [];
   public products: IProduct[] = [];
   private _listFilter: string = '';
-  get listFilter(): string {
+  public get listFilter(): string {
     return this._listFilter;
   }
-  set listFilter(value: string) {
+  public set listFilter(value: string) {
     this._listFilter = value;
     this.filteredProducts = this.performFilter(value);
   }
@@ -54,10 +54,6 @@ export class ProductListComponent implements OnInit, OnDestroy {
 
   public onAddToCartClicked(message1: any): void {
     console.log('ProductListComponent: ' + JSON.stringify(message1));
-  }
-
-  public onRatingClicked(message: string): void {
-    this.pageTitle = 'Product List: ' + message;
   }
 
   public ngOnDestroy(): void {
