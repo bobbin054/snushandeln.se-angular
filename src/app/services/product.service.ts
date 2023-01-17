8bb336d4faf997c17a351f45265f1330d460e508
Joakim Östelid <joakim.ostelid@outlook.com>
2023-01-17 22:02:37 +0100

    state

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,30 +1,28 @@
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, throwError } from 'rxjs';
-import { catchError, find, tap } from 'rxjs/operators';
+import { catchError } from 'rxjs/operators';
 import { IProduct } from '../interface/product';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProductService {
-  private productUrl = 'assets/products.json';
-  constructor(private http: HttpClient) {}
+  private _productUrl = 'assets/products.json';
+  constructor(private _httpClient: HttpClient) {}
 
   getProducts(): Observable<IProduct[]> {
-    return this.http
-      .get<IProduct[]>(this.productUrl)
+    return this._httpClient
+      .get<IProduct[]>(this._productUrl)
       .pipe(catchError(this.handleError));
   }
 
   handleError(errorResponse: HttpErrorResponse) {
-    let errorMessage = '';
     if (errorResponse.error instanceof ErrorEvent) {
-      errorMessage = `An error occurred: ${errorResponse.error.message}`;
-    } else {
-      errorMessage = `Server returned code: ${errorResponse.status}, error message is: ${errorResponse.message}`;
+      return throwError(`An error occurred: ${errorResponse.error.message}`);
     }
-    // console.error(errorMessage);
-    return throwError(errorMessage);
+    return throwError(
+      `Server returned code: ${errorResponse.status}, error message is: ${errorResponse.message}`
+    );
   }
 }
